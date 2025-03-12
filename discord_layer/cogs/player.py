ee34bdcb651974fc1a18d4dd2e22ed7d25fa2ab1
Eduardo Bachmann <eduardo.bachmann@idok.cl>
2025-03-12 18:02:44 -0300

    FEAT:PLAYER AS REGISTER ON DISCORD

--- discord_layer/cogs/player.py
+++ discord_layer/cogs/player.py
@@ -2,12 +2,14 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from typing import Optional
-from elorace.routers import player as player_router
-from elorace.routers import summoner as summoner_router
 from elorace.logger_config import get_logger
+from elorace.database import SessionLocal
+from elorace.models import player as player_model
+from elorace.schemas import PlayerCreate
 
 
 logger = get_logger(__name__)
+
 
 class PlayerCommands(commands.Cog):  
     def __init__(self, bot):
@@ -17,19 +19,21 @@ class PlayerCommands(commands.Cog):
     async def register(self, interaction: discord.Interaction):  
         """Register a new player"""
         logger.info(f"Registering player {interaction.user.name}")
-        try:
-            player_user = interaction.user.name
-            player_mail = interaction.user.email if hasattr(interaction.user, 'email') else None
-            player_data = {
-                "name": player_user,
-                "correo": player_mail  
-            }
-            response = await player_router.register_player(player_data)
-            logger.info(f"Player {response.name} registered successfully")
-            await interaction.response.send_message(f"Player {response.name} registered successfully")
-        except Exception as e:
-            logger.error(f"Error registering player: {e}")
-            await interaction.response.send_message("Error registering player", ephemeral=True)
+        player_user = interaction.user.name
+        player_source_id = interaction.user.id
+        player_source = 'DISCORD'
+        with SessionLocal() as db:
+            db_player = db.query(player_model).filter(
+                (player_model.name == player_user)
+                & (player_model.source_id == player_source_id)
+                & (player_model.source == player_source)
+            ).first()
+            if db_player:
+                logger.info("Player already exists")
+                await interaction.response.send_message("Player already exists")
+                return
+            
+
     
     @app_commands.command(name="register_invocator", description="Register a summoner")
     @app_commands.describe(
